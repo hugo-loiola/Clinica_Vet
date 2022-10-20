eb11284b8981e5370c46985e1f1d09d207d74ae7
Hugo Loiola <hugoloiola.1@gmail.com>
2022-10-20 12:07:52 -0300

    controller cliente

--- app/Controllers/Http/ClientesController.ts
+++ app/Controllers/Http/ClientesController.ts
@@ -1,3 +1,31 @@
+/* eslint-disable @typescript-eslint/explicit-member-accessibility */
 // import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
-export default class ClientesController {}
+import Cliente from 'App/Models/Cliente'
+
+export default class ClientesController {
+  async index() {
+    return await Cliente.query()
+  }
+  async store({ request }) {
+    const dados = request.only(['nome', 'endereco', 'telefone', 'cpf'])
+    return await Cliente.create(dados)
+  }
+  async show({ request }) {
+    const id = request.param('id')
+    return await Cliente.findOrFail(id)
+  }
+  async destroy({ request }) {
+    const id = request.param('id')
+    const cliente = await Cliente.findOrFail(id)
+    return await cliente.delete()
+  }
+  async update({ request }) {
+    const id = request.param('id')
+    const cliente = await Cliente.findOrFail(id)
+    const dados = request.only(['nome', 'endereco', 'telefone', 'cpf'])
+    await cliente.merge(dados).save()
+
+    return cliente
+  }
+}
