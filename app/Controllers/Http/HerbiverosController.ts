6030932f1b622d974829bf603650a3ea8106d28f
Hugo Loiola <hugoloiola.1@gmail.com>
2022-10-20 12:32:07 -0300

    controller herbivero

--- app/Controllers/Http/HerbiverosController.ts
+++ app/Controllers/Http/HerbiverosController.ts
@@ -1,3 +1,31 @@
+/* eslint-disable @typescript-eslint/explicit-member-accessibility */
 // import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
-export default class HerbiverosController {}
+import Herbivoro from 'App/Models/Herbivoro'
+
+export default class HerbiverosController {
+  async index() {
+    return await Herbivoro.query()
+  }
+  async store({ request }) {
+    const dados = request.only(['animalId', 'altura', 'peso'])
+    return await Herbivoro.create(dados)
+  }
+  async show({ request }) {
+    const id = request.param('id')
+    return await Herbivoro.findOrFail(id)
+  }
+  async destroy({ request }) {
+    const id = request.param('id')
+    const herbivero = await Herbivoro.findOrFail(id)
+    return await herbivero.delete()
+  }
+  async update({ request }) {
+    const id = request.param('id')
+    const herbivero = await Herbivoro.findOrFail(id)
+    const dados = request.only(['animalId', 'altura', 'peso'])
+    await herbivero.merge(dados).save()
+
+    return herbivero
+  }
+}
