4a95d8a9657e64ecace5d92fd854996d0a4a19f9
Hugo Loiola de Queiroz <hugoloiola.1@gmail.com>
2022-10-19 22:45:37 -0300

    controller animal

--- app/Controllers/Http/AnimalsController.ts
+++ app/Controllers/Http/AnimalsController.ts
@@ -1,3 +1,31 @@
+/* eslint-disable @typescript-eslint/explicit-member-accessibility */
 // import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
-export default class AnimalsController {}
+import Animal from 'App/Models/Animal'
+
+export default class AnimalsController {
+  async index() {
+    return await Animal.query()
+  }
+  async store({ request }) {
+    const dados = request.only(['clienteId', 'nome', 'tipo', 'raca', 'idade', 'alergia'])
+    return await Animal.create(dados)
+  }
+  async show({ request }) {
+    const id = request.param('id')
+    return await Animal.findOrFail(id)
+  }
+  async destroy({ request }) {
+    const id = request.param('id')
+    const aluno = await Animal.findOrFail(id)
+    return await aluno.delete()
+  }
+  async update({ request }) {
+    const id = request.param('id')
+    const animal = await Animal.findOrFail(id)
+    const dados = request.only(['clienteId', 'nome', 'tipo', 'raca', 'idade', 'alergia'])
+    await animal.merge(dados).save()
+
+    return animal
+  }
+}
