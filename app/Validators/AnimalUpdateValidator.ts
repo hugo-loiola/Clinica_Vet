f6db3d777d55fa6c22c9e1b66f0112cbb228b2e8
Hugo Loiola de Queiroz <hugoloiola.1@gmail.com>
2022-11-11 17:46:26 -0300

    update animal

--- app/Validators/AnimalUpdateValidator.ts
+++ app/Validators/AnimalUpdateValidator.ts
@@ -1,40 +1,47 @@
-import { schema, CustomMessages } from '@ioc:Adonis/Core/Validator'
+import { schema, CustomMessages, rules } from '@ioc:Adonis/Core/Validator'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class AnimalUpdateValidator {
   constructor(protected ctx: HttpContextContract) {}
+  public schema = schema.create({
+    nome: schema.string.nullableAndOptional([
+      rules.alpha({ allow: ['space'] }),
+      rules.maxLength(100),
+    ]),
 
-  /*
-   * Define schema to validate the "shape", "type", "formatting" and "integrity" of data.
-   *
-   * For example:
-   * 1. The username must be of data type string. But then also, it should
-   *    not contain special characters or numbers.
-   *    ```
-   *     schema.string({}, [ rules.alpha() ])
-   *    ```
-   *
-   * 2. The email must be of data type string, formatted as a valid
-   *    email. But also, not used by any other user.
-   *    ```
-   *     schema.string({}, [
-   *       rules.email(),
-   *       rules.unique({ table: 'users', column: 'email' }),
-   *     ])
-   *    ```
-   */
-  public schema = schema.create({})
+    tipo: schema.string.nullableAndOptional([
+      rules.alpha({ allow: ['space'] }),
+      rules.maxLength(50),
+    ]),
 
-  /**
-   * Custom messages for validation failures. You can make use of dot notation `(.)`
-   * for targeting nested fields and array expressions `(*)` for targeting all
-   * children of an array. For example:
-   *
-   * {
-   *   'profile.username.required': 'Username is required',
-   *   'scores.*.number': 'Define scores as valid numbers'
-   * }
-   *
-   */
-  public messages: CustomMessages = {}
+    raca: schema.string.nullableAndOptional([
+      rules.alpha({ allow: ['space'] }),
+      rules.maxLength(50),
+    ]),
+
+    idade: schema.number.nullableAndOptional(),
+
+    alergia: schema.string.nullableAndOptional([
+      rules.alpha({ allow: ['space'] }),
+      rules.maxLength(500),
+    ]),
+
+    cliente_id: schema.number.nullableAndOptional([
+      rules.unique({
+        table: 'clientes',
+        column: 'id',
+      }),
+      rules.exists({
+        table: 'clientes',
+        column: 'id',
+      }),
+    ]),
+  })
+
+  public messages: CustomMessages = {
+    maxLength: 'O número máximo de caractéres do campo {{ field }} é de {{ options.maxLength }}',
+    range: '{{ field }} tem que ser entre {{ options.range }}',
+    unique: '{{ field }} tem que ser único',
+    exists: 'O {{ field }} é obrigatório',
+  }
 }
