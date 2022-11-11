5feec4fe5f4ebceba37c92c6137f5e94dbda69bb
Hugo Loiola de Queiroz <hugoloiola.1@gmail.com>
2022-11-11 17:50:49 -0300

    update herbivoto

--- app/Validators/HerbivoroUpdateValidator.ts
+++ app/Validators/HerbivoroUpdateValidator.ts
@@ -1,40 +1,23 @@
-import { schema, CustomMessages } from '@ioc:Adonis/Core/Validator'
+import { schema, CustomMessages, rules } from '@ioc:Adonis/Core/Validator'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class HerbivoroUpdateValidator {
   constructor(protected ctx: HttpContextContract) {}
 
-  /*
-   * Define schema to validate the "shape", "type", "formatting" and "integrity" of data.
-   *
-   * For example:
-   * 1. The username must be of data type string. But then also, it should
-   *    not contain special characters or numbers.
-   *    ```
-   *     schema.string({}, [ rules.alpha() ])
-   *    ```
-   *
-   * 2. The email must be of data type string, formatted as a valid
-   *    email. But also, not used by any other user.
-   *    ```
-   *     schema.string({}, [
-   *       rules.email(),
-   *       rules.unique({ table: 'users', column: 'email' }),
-   *     ])
-   *    ```
-   */
-  public schema = schema.create({})
+  public schema = schema.create({
+    animal_id: schema.number.nullableAndOptional([
+      rules.unique({ table: 'animals', column: 'id' }),
+      rules.exists({ table: 'animals', column: 'id' }),
+    ]),
 
-  /**
-   * Custom messages for validation failures. You can make use of dot notation `(.)`
-   * for targeting nested fields and array expressions `(*)` for targeting all
-   * children of an array. For example:
-   *
-   * {
-   *   'profile.username.required': 'Username is required',
-   *   'scores.*.number': 'Define scores as valid numbers'
-   * }
-   *
-   */
-  public messages: CustomMessages = {}
+    altura: schema.number.nullableAndOptional([rules.range(1, 100)]),
+
+    peso: schema.number.nullableAndOptional([rules.range(0, 50)]),
+  })
+
+  public messages: CustomMessages = {
+    unique: '{{ field }} tem que ser único',
+    exists: 'O {{ field }} é obrigatório',
+    range: '{{ field }} pode ser de {{ options.range }}',
+  }
 }
